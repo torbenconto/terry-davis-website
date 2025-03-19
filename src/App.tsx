a1ab60729da21e2630a3fb5c373be9734b329569
torbenconto <torbenmconto@gmail.com>
2025-03-19 10:42:45 -0400

    update App component layout and improve Terry Davis description

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       {/* First Fullscreen Section */}
       <div className="w-full h-screen flex">
-        <img src={terry} className="p-24" alt="Terry Davis" />
+        <img src={terry} className="p-24 py-36" alt="Terry Davis" />
         <div className="flex flex-col justify-center items-center text-center">
           <h1 className="instrument-serif-regular text-9xl font-serif uppercase tracking-wide">Terry Davis 2024</h1>
           <h2 className="instrument-serif-regular text-5xl font-serif uppercase tracking-wider mt-4">
@@ -27,17 +27,14 @@ function App() {
           <video className="w-1/2" controls autoPlay>
             <source src={terryvid} type="video/mp4" />
           </video>
-          <div className="px-4">
+          <div className="px-12">
           <h1 className="instrument-serif-regular text-6xl font-serif uppercase tracking-wide">More About Terry</h1>
           <p className="mt-8 text-xl instrument-serif-regular">
-            Terry Davis, a visionary who saw beyond the conventional system. His work is a testament to unyielding creativity and dedication to a cause greater than himself.
+            Terry Davis, a computer programmer, was known for creating TempleOS, an operating system that he built entirely from scratch. He was a brilliant mind who struggled with mental health issues.
           </p>
           <p className="mt-6 text-lg instrument-serif-regular">
-            His journey is filled with obstacles, but his insights into truth and reality continue to inspire countless people.
+            His journey was filled with obstacles, but his reality continues to inspire countless people.
           </p>
-          <button className="mt-8 pt-serif-regular px-12 py-3 border-2 border-black text-black text-lg rounded-lg hover:shadow-lg transition-all duration-300">
-            Discover More
-          </button>
           </div>
         </div>
       </div>
